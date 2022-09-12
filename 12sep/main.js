efd76d9c7406b2ab94136f190f2cc17794ab5cf9
scarletalfaro <scarletalfaro9@gmail.com>
2022-09-12 10:46:09 -0300

    12sep

---
+++ 12sep/main.js
@@ -0,0 +1,17 @@
+let nombre // declarar
+nombre ="alejandro" // asignar una valor
+
+/* const permite declarar variables constantes,
+nose puede cabiar el valor en la ejecucion del programa */
+const pi =3.1416// declarando y asignando un valor 
+
+//let permite declarar una variable y luego cambiar su valor 
+let apellido
+
+nombre = "esteban"// sring
+apellido = "nuñez"
+
+const numero1 = 1//number
+const numero2 = 10
+
+console.log(numero1 + numero2)
